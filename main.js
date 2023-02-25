3437abfd5e05eb8f9b3eb0ca5e60a3771f8aa5d9
Friska Adisti Mahardini <friskamahardini@gmail.com>
2023-02-25 10:39:19 +0700

    add DOM for header

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-// navbar toggle
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
